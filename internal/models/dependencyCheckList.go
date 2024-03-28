ce868966a35bcbfa7fcfe97d8015767630e55e48
Eslam Allam <eslamallam73@gmail.com>
2024-03-28 09:48:05 +0300

    feat: Add dependency pagination

--- internal/models/dependencyCheckList.go
+++ internal/models/dependencyCheckList.go
@@ -10,122 +10,146 @@ import (
 var hoverStyle lipgloss.Style = lipgloss.NewStyle().Foreground(lipgloss.Color("205"))
 
 type item struct {
-    id string
-    name string
-    checked string
+	id      string
+	name    string
+	checked string
 }
 
 type itemGroup struct {
-    name string
-    items []item
+	name  string
+	items []item
 }
 
-type dependencyList struct {
-	Selected   map[int]struct{}
-	Groups     []itemGroup
-	Cursor     int
-	TotalItems int
+type model struct {
+	Selected    map[int]struct{}
+	Groups      []itemGroup
+	Cursor      int
+	TotalItems  int
+	currentPage int
+	pageSize    int
 }
 
 type Dependency struct {
-    Id string
-    Name string
-    GroupName string
+	Id        string
+	Name      string
+	GroupName string
 }
 
-func (m dependencyList) View() string {
+func (m model) View() string {
 	s := strings.Builder{}
-    
-    counter := 0
-	for i, dependencyGroup := range m.Groups {
-        if i > 3 {
-            break
-        }
-		s.WriteString(dependencyGroup.name)
-		s.WriteString("\n")
-		for _, item := range dependencyGroup.items {
+	currentPage := m.Cursor / m.pageSize
+	startingIndex := currentPage * m.pageSize
+	lastIndex := startingIndex + m.pageSize - 1
+	itemIndex := 0
+	for _, page := range m.Groups {
+		hasItems := false
+		firstItem := true
+		for _, item := range page.items {
+			if itemIndex < startingIndex {
+				itemIndex++
+				continue
+			}
 
-			if _, ok := m.Selected[counter]; ok{
+			if itemIndex > lastIndex {
+				break
+			}
+
+			if firstItem {
+				s.WriteString(page.name)
+				s.WriteString("\n")
+				firstItem = false
+			}
+			hasItems = true
+
+			if _, ok := m.Selected[itemIndex]; ok {
 				s.WriteString("[✓] ")
 			} else {
 				s.WriteString("[ ] ")
 			}
-            itemDisplay := item.name
-            if counter == m.Cursor {
-                itemDisplay = hoverStyle.Render(itemDisplay)
-            }
+			itemDisplay := item.name
+			if itemIndex == m.Cursor {
+				itemDisplay = hoverStyle.Render(itemDisplay)
+			}
 			s.WriteString(itemDisplay)
 			s.WriteString("\n")
-            counter++
+			itemIndex++
 		}
-		s.WriteString("\n")
+		if hasItems {
+			s.WriteRune('\n')
+		}
 	}
 	return s.String()
 }
 
-func (m dependencyList) Init() tea.Cmd  {
-    return nil
+func (m model) Init() tea.Cmd {
+	return nil
 }
 
-
-func (m dependencyList) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
+	// Is it a key press?
+	case tea.KeyMsg:
 
-    // Is it a key press?
-    case tea.KeyMsg:
+		// Cool, what was the actual key pressed?
+		switch msg.String() {
 
-        // Cool, what was the actual key pressed?
-        switch msg.String() {
+		// These keys should exit the program.
+		case "ctrl+c", "q":
+			return m, tea.Quit
 
-        // These keys should exit the program.
-        case "ctrl+c", "q":
-            return m, tea.Quit
+		// The "up" and "k" keys move the cursor up
+		case "up", "k":
+			if m.Cursor > 0 {
+				m.Cursor--
+			}
 
-        // The "up" and "k" keys move the cursor up
-        case "up", "k":
-            if m.Cursor > 0 {
-                m.Cursor--
-            }
+		// The "down" and "j" keys move the cursor down
+		case "down", "j":
+			if m.Cursor < m.TotalItems-1 {
+				m.Cursor++
+			}
 
-        // The "down" and "j" keys move the cursor down
-        case "down", "j":
-            if m.Cursor < m.TotalItems-1 {
-                m.Cursor++
-            }
+		// The "enter" key and the spacebar (a literal space) toggle
+		// the selected state for the item that the cursor is pointing at.
+		case "enter", " ":
 
-        // The "enter" key and the spacebar (a literal space) toggle
-        // the selected state for the item that the cursor is pointing at.
-        case "enter", " ":
+			if _, ok := m.Selected[m.Cursor]; ok {
+				delete(m.Selected, m.Cursor)
+			} else {
+				m.Selected[m.Cursor] = struct{}{}
+			}
+		}
+	}
 
-        if _, ok := m.Selected[m.Cursor]; ok {
-                delete(m.Selected, m.Cursor)
-            } else {
-                m.Selected[m.Cursor] = struct{}{}
-            }
-        }
-        }
-    
-
-    // Return the updated model to the Bubble Tea runtime for processing.
-    // Note that we're not returning a command.
-    return m, nil
+	// Return the updated model to the Bubble Tea runtime for processing.
+	// Note that we're not returning a command.
+	return m, nil
 }
 
 func NewModel(items ...Dependency) tea.Model {
-    itemMap := make(map[string][]item)
-    
-    for _, o := range items {
-        itemMap[o.GroupName] = append(itemMap[o.GroupName], item{
-            id: o.Id,
-            name: o.Name,
-        })
-    }
+	itemMap := make(map[string][]item)
 
-    model := dependencyList{TotalItems: len(items), Selected: make(map[int]struct{})}
+	for _, o := range items {
+		itemMap[o.GroupName] = append(itemMap[o.GroupName], item{
+			id:   o.Id,
+			name: o.Name,
+		})
+	}
 
-    for key, val := range itemMap {
-        model.Groups = append(model.Groups, itemGroup{name: key, items: val})
-    }
+	model := model{
+		TotalItems: len(items),
+		Selected:   make(map[int]struct{}),
+		pageSize:   5,
+		Groups:     make([]itemGroup, 0),
+	}
 
-    return model
+	for group, items := range itemMap {
+
+		if group == "" || len(items) == 0 {
+			continue
+		}
+		model.Groups = append(model.Groups, itemGroup{name: group, items: items})
+	}
+
+	return model
 }
